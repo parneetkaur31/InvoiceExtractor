2dfc8a3bb875d21c3f261d0c43c322e692ac4e29
parneetkaur31 <144652723+parneetkaur31@users.noreply.github.com>
2026-03-05 20:28:06 +0530

    counter field add

--- invoices/models.py
+++ invoices/models.py
@@ -9,7 +9,7 @@ class Invoice(models.Model):
     order_id = models.CharField(max_length=100, blank=True, null=True)
     gstin = models.CharField(max_length=20, blank=True, null=True)
     grand_total = models.CharField(max_length=50, blank=True, null=True)
-
+    status = models.CharField(max_length=20, default="processing")
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
